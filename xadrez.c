d1c019878e88e19fcb2af1986c743f71cd08bb1e
Christian Costa <136290447+iTz-Chris@users.noreply.github.com>
2025-03-30 18:50:48 -0300

    Update xadrez.c - Nível Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,64 +1,85 @@
 #include <stdio.h>
 
+// Função para movimento da Torre:
+
+void torre(int numero) {
+    if (numero > 0) {
+        printf ("Direita\n");
+        torre (numero - 1);
+    }
+}
+
+// Função para movimento do Bispo:
+
+void bispo(int numero) {
+    if (numero > 0) {
+
+        // Loop externo
+        for (int i = 1; i <= 1; i++) {
+
+            // Loop interno
+            for (int j = 1; j <=1; j++) {
+                printf("Cima\n");
+                printf("Direita\n");
+
+            }
+        }
+        
+        bispo (numero - 1);
+    }
+}
+
+// Função para movimento da Rainha:
+
+void rainha(int numero) {
+    if (numero > 0) {
+        printf("Esquerda\n");
+        rainha (numero - 1);
+    }
+}
+
+
 int main() {
 
+    printf ("Torre (5 casas para a direita): \n");
+    torre (5);
+    printf ("\n"); 
 
-    int torre = 5, bispo = 5, rainha = 8;
-    int cavaloBaixo, cavaloEsquerda = 1;
-    
-    
-    // Movimento da Torre:
-    printf ("Torre (5 casas para a direita):\n");
+    printf ("Bispo (5 casas na diagonal para cima e à direita): \n");
+    bispo (5);
+    printf ("\n");
 
-    for (int i = 0; i < torre; i++) {
-        printf("Direita\n");
-    }
-
-
-    // Movimento do Bispo
-    printf("\n");
-    printf("Bispo (5 casas na diagonal para cima e à direita):\n");
-
-    int i = 0;
-    while (i < bispo) {
-
-        printf("Cima\n");
-        printf("Direita\n");
-        i++;
-    }
+    printf ("Rainha (8 casas para a esquerda): \n");
+    rainha (8);
+    printf ("\n");
 
 
-    // Movimento da Rainha
-    printf("\n");
-    printf("Rainha (8 casas para a esquerda):\n");
-
-    int incremento = 0;
-    do {
-        printf("Esquerda\n");
-        incremento++;
-        
-    } while (incremento < rainha);
+    int cavaloCima, cavaloDireita;
 
 
     // Movimento do Cavalo
     printf ("\n");
-    printf ("Cavalo (duas casas para baixo e uma casa para a esquerda):\n");
+    printf ("Cavalo (duas casas para cima e uma casa para a direita): \n");
 
-    for (cavaloBaixo = 1; cavaloBaixo <= 3; cavaloBaixo++) {
+    // Loop externo para o movimento de duas casas para cima:
+    for (cavaloCima = 1, cavaloDireita = 1; cavaloCima <= 2 && cavaloDireita <= 1; cavaloCima++) {
 
-        while (cavaloEsquerda <= 1)
-        {
-            printf ("Baixo\n");
-            printf ("Baixo\n");
-            printf ("Esquerda\n");
-            cavaloEsquerda++;
-        }
+    // loop interno para o movimento de uma casa para a direita:
+    for (cavaloDireita = 1; cavaloDireita <= 1; cavaloDireita++) {
+
+        printf("Cima\n"); 
+        printf("Cima\n"); 
+        printf("Direita\n"); 
+
+    }
           
             printf ("\n");
+
     }
 
 
-    
-return 0;
 
+
+
+    return 0;
 }
